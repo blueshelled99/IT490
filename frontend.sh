199a4126c547e5b9d3a2c85dfc16584a6357aadb
maf64 <60161200+maf64@users.noreply.github.com>
2020-02-27 18:41:09 -0500

    Add files via upload

---
+++ frontend.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install python3
+sudo apt update
+sudo apt install python3-pip
+pip3 --version
+sudo apt-get update 
+sudo apt-get install python-pika
+sudo apt-install vim
+pip install flask
+sudo apt-get update
+sudo apt-get install apache2
