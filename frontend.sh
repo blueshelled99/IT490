034d88679317c2c686089d929615076c4d947f8d
maf64 <60161200+maf64@users.noreply.github.com>
2020-02-27 19:04:37 -0500

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -6,6 +6,15 @@ pip3 --version
 sudo apt-get update 
 sudo apt-get install python-pika
 sudo apt-install vim
-pip install flask
 sudo apt-get update
 sudo apt-get install apache2
+cd ~
+sudo apt-get install python-virtualenv 
+sudo apt-get install python-pip
+virtualenv --version
+mkdir flask-application
+cd flask-application
+virtualenv flask-env
+source flask-env/bin/activate
+pip install flask
+
