fe74f677654d33c2dc95d6eb60f9cfe7e783cce1
blueshelled99 <60161203+blueshelled99@users.noreply.github.com>
2020-02-29 15:02:34 -0500

    Update frontend.sh
    line 8: using pip3 to install pika to ensure concurrency
    line 19: adding sudo just in case

--- frontend.sh
+++ frontend.sh
@@ -2,11 +2,11 @@ sudo apt-get update
 sudo apt-get install python3
 python3 --version
 sudo apt update
-sudo apt install python3-pip
+sudo apt-get install python3-pip
 pip3 --version
 sudo apt-get update 
-sudo apt-get install python3-pika
-sudo apt-install vim
+sudo pip3 install pika
+sudo apt-get install vim
 sudo apt-get update
 sudo apt-get install apache2
 cd ~
@@ -16,6 +16,5 @@ mkdir flask-application
 cd flask-application
 virtualenv flask-env
 source flask-env/bin/activate
-pip3 install flask
+sudo pip3 install flask
 python3 FRONT_END.py
-
