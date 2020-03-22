c0dc0877a30a7ff0cb9b35a82bb71a1074bb890d
blueshelled99 <60161203+blueshelled99@users.noreply.github.com>
2020-03-21 20:40:17 -0400

    Create pyserver.py
    this file was used to test login the first time around after creating the login.php file

---
+++ back end/pyserver.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import pika
+
+credentials = pika.PlainCredentials('rabbitmq-test', 'test')
+parameters = pika.ConnectionParameters('192.168.1.50',
+				       5672,
+				       '/',
+				       credentials)
+
+connection = pika.BlockingConnection(parameters)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='user-test2', durable = True)
+
+
+def auth(n):
+    return "true"
+
+
+def on_request(ch, method, props, body):
+    n = body
+
+    print(" [.] auth(%s)" % n)
+    response = auth(n)
+
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=str(response))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='user-test2', on_message_callback=on_request)
+
+print(" [x] Awaiting RPC requests")
+channel.start_consuming()
