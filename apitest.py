1a6f41320db58ecea81fce3954a6395c396a5434
blueshelled99 <60161203+blueshelled99@users.noreply.github.com>
2020-03-22 22:51:11 -0400

    Create apitest.py
    testing the reading of json for api data

---
+++ apitest.py
@@ -0,0 +1,13 @@
+//r6K96zZiE3CiSz10AhkCh0EGSpKNbxmDYD4osUAN
+
+//https://api.open.fec.gov/v1/candidates/search/?sort_nulls_last=false&sort=name&api_key=DEMO_KEY&sort_null_only=false&page=1&sort_hide_null=false&per_page=20&name=Trump
+
+/*import urllib, json
+
+      url = "http://maps.googleapis.com/maps/api/geocode/json?address=googleplex&sensor=false"
+
+      response = urllib.urlopen(url)
+
+      data = json.loads(response.read())
+
+      print data*/
