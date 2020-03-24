630940af24ac2eb2cc3714f5a66e4c6dba806a48
Rishi Lalbahadur <35934715+rkl9@users.noreply.github.com>
2020-03-23 23:11:28 -0400

    Update rpc_server_login.py

--- back end/rpc_server_login.py
+++ back end/rpc_server_login.py
@@ -38,7 +38,7 @@ def login_user(login_arguments):
 	
 
 credentials = pika.PlainCredentials('rabbitmq-test', 'test')
-parameters = pika.ConnectionParameters('192.168.1.48,
+parameters = pika.ConnectionParameters('192.168.1.48',
 			5672,
 			'/',
 			credentials)
