842b0accfcc890b556ab17a8ffa2139b2ebbfd0e
blueshelled99 <60161203+blueshelled99@users.noreply.github.com>
2020-04-09 00:10:16 -0400

    Update user_home.php
    updated with a barebones version to get an image on how we will take queries using html forms and remembering what user is logged in

--- front end/user_home.php
+++ front end/user_home.php
@@ -1,12 +1,8 @@
 <?php
-session_start();
-
-if (! $_SESSION['logged_in']) {
+session_start();if (! $_SESSION['logged_in']) {
 	header("Location: index.html");
-}
-
-$email = $_POST['email'];
-$pass = $_POST['password'];
+}$user = $_SESSION['email'];
+//echo "<h1>$user</h1>";
 
 ?>
 
@@ -18,19 +14,22 @@ $pass = $_POST['password'];
     <link rel="stylesheet" href="login.css">
 </head>
 <body>
-<main>
-   
-        <div class="Square">
-
-        <center><h1>Home</h1>
-		<h2>Welcome, $email</h2>
-		<br>
-		<h2>What would you like to do?<h2>
+<main>        
+	<div class="Square">        
+	<center><h1>Home</h1>
+		<h2>What would you like to do?</h2>
         <br>
-            
-        </center>
-       
-    </div>
+	<h1><?php echo $user; ?></h1>
+	<form action ="" method="post">
+                <table>
+                    <tr>
+                        <td>E-mail:</td>
+                        <td><input type="text" id="email" name="email" required minlength="3" maxlength="20" value="<?php echo $user; ?>" readonly></td> <!-- you should add the hidden attribute to the input tag here -->
+		    </tr>              
+		</table>
+                <input type="submit" name="submit" value="submit query">
+	</form>    
+	</div>
 </main>
 </body>
 </html>
