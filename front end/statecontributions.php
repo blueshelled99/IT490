1fdbd750eb896d9b73ec33f6a40a3f92ebd1434a
blueshelled99 <60161203+blueshelled99@users.noreply.github.com>
2020-04-18 02:01:11 -0400

    Create statecontributions.php

---
+++ front end/statecontributions.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+
+$_SESSION['email'];
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$userSubmittal = array(
+	"user" => $_SESSION['email'],
+	"query" => "https://api.open.fec.gov/v1/presidential/contributions/by_state/?sort_hide_null=false&api_key=r6K96zZiE3CiSz10AhkCh0EGSpKNbxmDYD4osUAN&per_page=100&sort_null_only=false&sort=-contribution_receipt_amount&page=1&election_year=2020&sort_nulls_last=false"
+);
+
+$msgJson = json_encode($userSubmittal);
+
+$connection = new AMQPStreamConnection('192.168.1.50', 5672, 'rabbitmq-test', 'test'); //change ip address
+
+$channel = $connection->channel();
+
+$channel -> queue_declare('contribution_query_queue', false, true, false, false);
+
+$msg = new AMQPMessage($msgJson);
+
+$channel->basic_publish($msg, '', 'contribution_query_queue');
+
+$channel->close();
+$connection->close();
+?>
+
+<html>
+  <head>
+    <link rel="stylesheet" href="login.css">
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript">
+	function drawStatesMap() {
+	var options = {region: 'US', resolution: 'provinces'};
+        var dimension = "contribution_receipt_amount";
+            $.ajax({
+              url: "https://api.open.fec.gov/v1/presidential/contributions/by_state/?sort_hide_null=false&api_key=r6K96zZiE3CiSz10AhkCh0EGSpKNbxmDYD4osUAN&per_page=100&sort_null_only=false&sort=-contribution_receipt_amount&page=1&election_year=2020&sort_nulls_last=false",
+              dataType: "JSON"
+            }).done(function(data) {
+                    var statesArray = [["State",dimension]];
+                    $.each(data.results, function() {
+                        var stateitem = [this.contribution_state, this[dimension]];
+                        statesArray.push(stateitem);
+                    });
+              var statesData = google.visualization.arrayToDataTable(statesArray);
+              var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
+              chart.draw(statesData, options);
+              $("h3").append(" Sorted by  "+dimension);
+            });
+    }
+google.load('visualization', '1', {'packages': ['geochart']});
+google.setOnLoadCallback(drawStatesMap);
+    </script>
+  </head>
+  <body>
+    <h1>Here you go, <?php echo $_SESSION['email'] ?></h1>
+    <br>
+    <h1>OpenFEC's Top 100 Presidential Contributions by State</h1>
+    <div id="chart_div" style="width: 900px; height: 500px;" ></div>
+  </body>
+</html>
