9269278638a955c25551eae060140e79b8903e51
blueshelled99 <60161203+blueshelled99@users.noreply.github.com>
2020-04-18 12:31:24 -0400

    Update piecharts.php
    added comments to show what i changed
    but it should be self-evident after reading it over along with the documentation for charts in google charts

--- front end/piecharts.php
+++ front end/piecharts.php
@@ -35,25 +35,25 @@ $connection->close();
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script type="text/javascript">
-	function piedraw1() {
+	function piedraw1() { //make function here and name it
 	var options = {title: "OpenFEC's Aggregated Candidates Disbursements grouped by office for 2020"};
-        var dimension = "total_disbursements";
+        var dimension = "total_disbursements"; //what you want to use as an index?
             $.ajax({
               url: "https://api.open.fec.gov/v1/candidates/totals/by_office/?sort_hide_null=false&api_key=r6K96zZiE3CiSz10AhkCh0EGSpKNbxmDYD4osUAN&per_page=100&sort_null_only=false&page=1&is_active_candidate=true&election_year=2020&sort_nulls_last=true",
               dataType: "JSON"
             }).done(function(data) {
-                    var disbursementArray = [["Office",dimension]];
-                    $.each(data.results, function() {
-                        var item = [this.office, this[dimension]];
+                    var disbursementArray = [["Office",dimension]]; //array 0 can be renamed
+                    $.each(data.results, function() { //results is taken from the json; used to index
+                        var item = [this.office, this[dimension]]; //array[0] = category, array[1] = data for that category
                         disbursementArray.push(item);
                     });
               var disbursementData = google.visualization.arrayToDataTable(disbursementArray);
-              var chart = new google.visualization.PieChart(document.getElementById('disbursements'));
+              var chart = new google.visualization.PieChart(document.getElementById('disbursements')); //make sure to change chart type if you are doing another type
               chart.draw(disbursementData, options);
             });
     }
 google.charts.load('current', {'packages':['corechart']});
-google.setOnLoadCallback(piedraw1);
+google.setOnLoadCallback(piedraw1); //call the function here, in this case piedraw1
     </script>
     <script type="text/javascript">
 	function piedraw2() {
