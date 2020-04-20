f6b7365b5880155e2602bf0eaa4fb0656f09b11c
nickrogal <51394648+nickrogal@users.noreply.github.com>
2020-04-19 20:30:57 -0400

    Add files via upload
    Added comments. Removed line 29. Changed RabbitMQ message brokering process away from direct queue publishing to publishing to an Exchange and routing via Routing Key.

--- front end/UserRegistration.php
+++ front end/UserRegistration.php
@@ -17,34 +17,35 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-//adding code for encryption here maybe
+//encrypt user creds
 $options = [ 'salt' => 'seasalt_icecream123456' ];
 $hashed_password = password_hash($_POST['password'], PASSWORD_BCRYPT, $options);
-	
+
+#POST Data	
 $userSubmittal = array(
 	"firstName" => $_POST['firstName'],
 	"lastName" => $_POST['lastName'],
 	"email" => $_POST['email'],
-	"pass" => $hashed_password //comment this and uncomment the next line to get everything back to normal
-	//"pass" => $_POST['password']
+	"pass" => $hashed_password
 );
 
+#encode POST data to JSON
 $msgJson = json_encode($userSubmittal);
 
-
-
+#AMQP Connection
 $connection = new AMQPStreamConnection('10.0.0.7', 5672, 'rabbitmq-service', 'Team666!'); //change ip address
-
 $channel = $connection->channel();
 
-$channel -> queue_declare('register-queue', false, true, false, false);
-
+#RabbitMQ Message data as JSON
 $msg = new AMQPMessage($msgJson);
 
-$channel->basic_publish($msg, '', 'register-queue');
+#Declare Exchange and routing key
+$channel->basic_publish($msg, 'Registration-Exchange', 'send-user-registration');
 
+#Echo result
 echo "Account was registered. Return to previous page to login.";
 
+#Close connection
 $channel->close();
 $connection->close();
 
