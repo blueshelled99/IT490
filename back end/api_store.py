ce3ae9bc984a83b0e32f02e90b551c73e1fdb372
Rishi Lalbahadur <35934715+rkl9@users.noreply.github.com>
2020-05-04 18:11:27 -0400

    Update api_store.py

--- back end/api_store.py
+++ back end/api_store.py
@@ -46,13 +46,7 @@ def on_request(ch, method, props, body):
 
 	print(n)
 	auth(n)
-	'''print(" [x] Sent " + response)
-	ch.basic_publish(exchange='Login-Exchange',
-					 routing_key='login-response',
-					 properties=pika.BasicProperties(correlation_id = \
-														 props.correlation_id),
-					 body=str(response))
-	ch.basic_ack(delivery_tag=method.delivery_tag)'''
+
 
 
 channel.basic_qos(prefetch_count=1)
